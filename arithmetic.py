b678a1ab6a2b8f95744f3c2e1743d7d8b576ba38
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-06-02 18:42:14 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,23 @@
+num1 = float(input('Enter first number : '))
+num2 = float(input('Enter second number : '))
+
+# Addition
+add = num1+num2
+# Subtract
+sub = num1-num2
+# Multiply
+mult = num1*num2
+# Divide
+div = num1/num2
+# Modulus
+mod = num1%num2
+# Exponent
+expo = num1**num2
+
+print(f'The sum of {num1} and {num2} is {add}')
+print(f'The subtract of {num1} and {num2} is {sub}')
+print(f'The product of {num1} and {num2} is {mult}')
+print(f'The division of {num1} and {num2} is {div}')
+print(f'The modulus of {num1} and {num2} is {mod}')
+print(f'The exponent of {num1} and {num2} is {expo}')
+
---
+++ sub_two_num.py
@@ -0,0 +1,7 @@
+def sub(a,b):
+    return (a-b)
+
+a = int(input('Enter first number : '))
+b = int(input('Enter second number : '))
+
+print(f'The result of subtracting {a} and {b} is {sub(a,b)}')
